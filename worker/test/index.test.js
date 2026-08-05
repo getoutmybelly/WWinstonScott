2ff77e466ac37f26af2ccefba73c7471b2e0d7dc
getoutmybelly <wayneescottii@gmail.com>
2026-08-05 01:18:15 -0500

    Test Winston Facebook profile target

--- worker/test/index.test.js
+++ worker/test/index.test.js
@@ -50,7 +50,7 @@ test("publisher stays locked until the admin password is supplied", async () => 
   const body = await unlocked.text();
   assert.match(body, /LinkedIn is not connected/);
   assert.match(body, /Copy &amp; Open Personal Facebook/);
-  assert.match(body, /Facebook requires you to make the final post on a personal profile/);
+  assert.match(body, /Facebook requires you to make the final post on a personal profile/);\n  assert.match(body, /https:\\/\\/www\\.facebook\\.com\\/WayneScottII/);
 });
 
 test("same-origin login works when the browser serializes Origin as null", async () => {
